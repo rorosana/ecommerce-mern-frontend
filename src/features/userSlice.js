74283ad5d4da1361cb81909f208c80455d785bb1
rorosana <r.a.caraballo8@gmail.com>
2023-07-10 13:01:12 +0200

    "using redux and Slice"

---
+++ src/features/userSlice.js
@@ -0,0 +1,13 @@
+import {createSlice} from '@reduxjs/toolkit';
+//appAPI
+//import appApi from '../services/appApi';
+
+const initialState = null;
+
+export const userSlice = createSlice ({
+    name: 'user',
+    initialState,
+    reducers: {},
+})
+
+export default userSlice.reducer;
