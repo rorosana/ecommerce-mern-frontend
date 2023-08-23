bb0eb41c40d7b071e0f90014b10c8dd404677d5f
rorosana <r.a.caraballo8@gmail.com>
2023-08-23 02:22:29 +0200

    "create new account done"

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,13 +1,17 @@
 import {createSlice} from '@reduxjs/toolkit';
 //appAPI
-//import appApi from '../services/appApi';
+import appAPI from '../services/appAPI';
 
 const initialState = null;
 
 export const userSlice = createSlice ({
-    name: 'user',
+    name: "products",
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder.addMatcher(appAPI.endpoints.signup.matchFulfilled, (_, {payload}) => payload);
+        builder.addMatcher(appAPI.endpoints.login.matchFulfilled, (_, {payload}) => payload);
+    },
 })
 
 export default userSlice.reducer;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,17 +1,38 @@
 import React, {useState} from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import '../css/Signup.css';
+import {useSignupMutation} from '../services/appAPI';
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [signup, {error, isLoading, isError}] = useSignupMutation(); 
+
+  function handleSignup(e){
+      e.preventDefault();
+      signup({name, email, password});
+  }
+
   return (
     <Container>
       <Row>
         <Col md={6} className="signup__form--container">
-          <Form style={{ width: "100%" }}>
+          <Form style={{ width: "100%" }} onSubmit={handleSignup}>
             <h1>Login to your account</h1>
+            {isError && <Alert variant="danger">{error.data}</Alert>}
+            <Form.Group>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Your name"
+                value={name}
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
             <Form.Group>
               <Form.Label>Email Address</Form.Label>
               <Form.Control
@@ -35,10 +56,10 @@ function Signup() {
             </Form.Group>
 
             <Form.Group>
-              <Button type="submit">Login</Button>
+              <Button type="submit" disabled={isLoading}>Create account</Button>
             </Form.Group>
             <p>
-              Don't have an account?<Link to="/signup">Create account</Link>
+              Don't have an account?<Link to="/login">Login</Link>
             </p>
           </Form>
         </Col>
