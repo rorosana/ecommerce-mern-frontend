67cb68d91dd030da752a15037c342fafa344f519
rorosana <r.a.caraballo8@gmail.com>
2023-08-27 01:58:49 +0200

    logout done

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -7,11 +7,15 @@ const initialState = null;
 export const userSlice = createSlice ({
     name: "products",
     initialState,
-    reducers: {},
+    reducers: {
+        logout: () => initialState,
+    },
     extraReducers: (builder) => {
         builder.addMatcher(appAPI.endpoints.signup.matchFulfilled, (_, {payload}) => payload);
         builder.addMatcher(appAPI.endpoints.login.matchFulfilled, (_, {payload}) => payload);
     },
-})
+});
+
+export const {logout} = userSlice.actions;
 
 export default userSlice.reducer;
